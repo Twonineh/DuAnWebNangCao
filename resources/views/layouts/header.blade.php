39c3d5441f7fd7027dbb5184001a2d299c6fee82
Twonineh <t.anh25092003@gmail.com>
2025-02-28 10:10:11 +0700

    update

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,277 @@
+<header id="header" class="header" style="background-color: white;">
+<style>
+.nav-item a {
+  display: inline-block;
+  padding: 10px 15px;
+  border-radius: 15px;
+  text-align: center;
+  text-decoration: none;
+  color: black;
+  transition: all 0.3s ease;
+}
+
+.nav-item a:hover {
+  background-color: #ffd700; /* Màu vàng */
+  color: white; /* Màu chữ khi hover */
+  box-shadow: 0px 0px 10px 2px #ffd700; /* Hiệu ứng ánh sáng bao quanh */
+}
+</style>
+  <div class="container">
+    <div class="row display-flex" style="background-color: white;">
+      <div class="col-md-2 margin-auto trigger-menu">
+
+        <button type="button" class="navbar-toggle collapsed visible-xs" id="trigger-mobile">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <div class="logo">
+          <h2 style=" font-size: 35px; padding-bottom: 10px" class="logo-wrapper" href="{{ route('home_page') }}" title="{{ config('app.name') }}"><strong>Store</strong></h2>
+        </div>
+      </div>
+      <div class="col-md-3 margin-auto">
+        <div class="search">
+          <form class="search-bar" action="{{ route('search') }}" method="get" accept-charset="utf-8">
+            <input class="input-search" type="search" name="search_key" placeholder="{{ __('header.Search') }}" autocomplete="off">
+            <button type="submit"><i class="fas fa-search"></i></button>
+          </form>
+        </div>
+      </div>
+      <div class="col-md-7 hd-bg-white main-menu-responsive" style="padding-top: 10px;">
+        <div class="main-menu">
+          <div class="nav">
+            <ul style="display: flex; justify-content: space-around; padding: 10px; border-radius: 15px; background-color: #f8f9fa;">
+              <li class="nav-item {{ Helper::check_active(['home_page']) }}">
+                <a href="{{ route('home_page') }}" title="{{ __('header.Home') }}" style="padding: 10px 15px; border-radius: 10px; color:black">
+                  <span class="fas fa-home"></span>
+                  {{ __('header.Home') }}
+                </a>
+              </li>
+              <li class="nav-item {{ Helper::check_active(['about_page']) }}">
+                <a href="{{ route('about_page') }}" title="{{ __('header.About') }}" style="padding: 10px 15px; border-radius: 10px;">
+                  <span class="fas fa-info"></span>
+                  {{ __('header.About') }}
+                </a>
+              </li>
+              <li class="nav-item dropdown {{ Helper::check_active(['products_page', 'producer_page', 'product_page']) }}">
+                <a href="{{ route('products_page') }}" title="{{ __('header.Products') }}" style="padding: 10px 15px; border-radius: 10px;">
+                  <span class="fas fa-mobile-alt"></span>
+                  {{ __('header.Products') }}
+                </a>
+              </li>
+              <li class="nav-item {{ Helper::check_active(['posts_page', 'post_page']) }}">
+                <a href="{{ route('posts_page') }}" title="{{ __('header.News') }}" style="padding: 10px 15px; border-radius: 10px;">
+                  <span class="far fa-newspaper"></span>
+                  {{ __('header.News') }}
+                </a>
+              </li>
+              <li class="nav-item {{ Helper::check_active(['contact_page']) }}">
+                <a href="{{ route('contact_page') }}" title="{{ __('header.Contact') }}" style="padding: 10px 15px; border-radius: 10px;">
+                  <span class="fas fa-id-card"></span>
+                  {{ __('header.Contact') }}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="accout-menu">
+            @if(Auth::guest())
+              <div class="not-logged-menu">
+                <ul>
+                  <li class="menu-item {{ Helper::check_active(['login']) }}">
+                    <a href="{{ route('login') }}" title="{{ __('header.Login') }}">
+                      <span class="fas fa-user"></span>
+                      {{ __('header.Login') }}
+                    </a>
+                  </li>
+                  <li class="menu-item {{ Helper::check_active(['register']) }}">
+                    <a href="{{ route('register') }}" title="{{ __('header.Register') }}">
+                      <span class="fas fa-key"></span>
+                      {{ __('header.Register') }}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            @else
+              <div class="logged-menu">
+                <ul>
+                  <li class="menu-item dropdown {{ Helper::check_active(['orders_page', 'order_page', 'show_user', 'edit_user']) }}">
+                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" title="{{ Auth::user()->name }}">
+                      <div class="avatar" style="background-image: url('{{ Helper::get_image_avatar_url(Auth::user()->avatar_image) }}');"></div>
+                    </a>
+                    <ul class="dropdown-menu">
+                      @if(Auth::user()->admin)
+                      <li><a href="{{ route('admin.dashboard') }}"><i class="fas fa-tachometer-alt"></i> Quản Lý Website</a></li>
+                      @else
+                      <li class="{{ Helper::check_active(['orders_page', 'order_page']) }}">
+                        <a href="{{ route('orders_page') }}">
+                          <i class="fas fa-clipboard-list"></i> Quản Lý Đơn Hàng
+                        </a>
+                      </li>
+                      <li class="{{ Helper::check_active(['show_user', 'edit_user']) }}">
+                        <a href="{{ route('show_user') }}">
+                          <i class="fas fa-user-cog"></i> Quản Lý Tài Khoản
+                        </a>
+                      </li>
+                      @endif
+                      <li>
+                        <a id="logout" action="#"><i class="fas fa-power-off"></i> {{ __('header.Logout') }}</a>
+                      </li>
+                    </ul>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                  </li>
+                </ul>
+              </div>
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header><!-- /header -->
+
+<div class="backdrop__body-backdrop___1rvky"></div>
+<div class="mobile-main-menu">
+  <div class="mobile-main-menu-top">
+    <div class="mb-menu-top-header">
+      <div class="mb-menu-logo">
+        <a class="logo-wrapper" href="{{ route('home_page') }}" title="{{ config('app.name') }}">
+          <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}">
+        </a>
+      </div>
+      <button type="button" id="close-trigger-mobile">
+        <i class="fas fa-times"></i>
+      </button>
+    </div>
+    @if(Auth::guest())
+    <div class="mb-menu-top-body">
+      <div class="mb-menu-user">
+        <div style="background-image: url('{{ asset('images/no_login.svg') }}');"></div>
+      </div>
+      <div class="mb-menu-info">
+        <div class="info-top">Đăng Nhập</div>
+        <div class="info-bottom">Để nhận nhiều ưu đãi hơn</div>
+      </div>
+    </div>
+    <div class="mb-menu-top-footer">
+      <div class="mb-menu-action">
+        <a href="{{ route('login') }}" class="btn btn-success">
+          <i class="fas fa-user"></i> Đăng Nhập
+        </a>
+      </div>
+      <div class="mb-menu-action">
+        <a href="{{ route('register') }}" class="btn btn-warning">
+          <i class="fas fa-key"></i> Đăng Ký
+        </a>
+      </div>
+    </div>
+    @else
+    <div class="mb-menu-top-body">
+      <div class="mb-menu-user">
+        <div style="background-image: url('{{ Helper::get_image_avatar_url(Auth::user()->avatar_image) }}');"></div>
+      </div>
+      <div class="mb-menu-info">
+        <div class="info-top">{{ Auth::user()->name }}</div>
+        <div class="info-bottom">{{ Auth::user()->email }}</div>
+      </div>
+    </div>
+    <div class="mb-menu-top-footer">
+      @if(Auth::user()->admin)
+      <div class="mb-menu-action">
+        <a href="{{ route('admin.dashboard') }}" class="btn btn-success">
+          <i class="fas fa-tachometer-alt"></i> Dashboard
+        </a>
+      </div>
+      <div class="mb-menu-action">
+        <a id="mobile-logout" href="javascript:void(0);" class="btn btn-danger">
+          <i class="fas fa-power-off"></i> {{ __('header.Logout') }}
+        </a>
+      </div>
+      @else
+      <div class="mb-menu-action">
+        <a href="{{ route('show_user') }}" class="btn btn-success">
+          <span class="fas fa-user-cog"></span> Tài Khoản
+        </a>
+      </div>
+      <div class="mb-menu-action">
+        <a id="mobile-logout" href="javascript:void(0);" class="btn btn-danger">
+          <i class="fas fa-power-off"></i> {{ __('header.Logout') }}
+        </a>
+      </div>
+      @endif
+    </div>
+    @endif
+  </div>
+  <div class="mobile-main-menu-middle">
+    <div class="mb-menu-middle-header">
+      <h3>DANH MỤC</h3>
+    </div>
+    <div class="mb-menu-middle-body">
+      <ul>
+        <li class="mb-nav-item {{ Helper::check_active(['home_page']) }}">
+          <a href="{{ route('home_page') }}" title="{{ __('header.Home') }}">
+            <span class="fas fa-home"></span>
+            {{ __('header.Home') }}
+          </a>
+        </li>
+        <li class="mb-nav-item {{ Helper::check_active(['about_page']) }}">
+          <a href="{{ route('about_page') }}" title="{{ __('header.About') }}">
+            <span class="fas fa-info"></span>
+            {{ __('header.About') }}
+          </a>
+        </li>
+        <li class="mb-nav-item has-item-child {{ Helper::check_active(['products_page', 'producer_page', 'product_page']) }}">
+          <a href="{{ route('products_page') }}" title="{{ __('header.Products') }}">
+            <span class="fas fa-mobile-alt"></span>
+            {{ __('header.Products') }}
+          </a>
+          <button id="action-collapse" data-toggle="collapse" data-target="#item-child-collapse"><i class="fas fa-plus"></i></button>
+          <div id="item-child-collapse" class="collapse">
+            <ul>
+              @foreach($producers as $producer)
+                <li class="{{ Helper::check_param_active('producer_page', $producer->id) }}">
+                  <a href="{{ route('producer_page', ['id' => $producer->id]) }}" title="{{ $producer->name }}">
+                    {{ $producer->name }}
+                  </a>
+                </li>
+              @endforeach
+            </ul>
+          </div>
+        </li>
+        <li class="mb-nav-item {{ Helper::check_active(['posts_page', 'post_page']) }}">
+          <a href="{{ route('posts_page') }}" title="{{ __('header.News') }}">
+            <span class="far fa-newspaper"></span>
+            {{ __('header.News') }}
+          </a>
+        </li>
+        <li class="mb-nav-item {{ Helper::check_active(['contact_page']) }}">
+          <a href="{{ route('contact_page') }}" title="{{ __('header.Contact') }}">
+            <span class="fas fa-id-card"></span>
+            {{ __('header.Contact') }}
+          </a>
+        </li>
+        @if(Auth::check() && !Auth::user()->admin)
+        <li class="mb-nav-item {{ Helper::check_active(['orders_page', 'order_page']) }}">
+          <a href="{{ route('orders_page') }}">
+            <span class="fas fa-clipboard-list"></span> Đơn Hàng
+          </a>
+        </li>
+        @endif
+      </ul>
+    </div>
+  </div>
+  <div class="mobile-main-menu-bottom">
+    <div class="mobile-support">
+      <div class="text-support">HỖ TRỢ</div>
+      <div class="info-support">
+        <i class="fa fa-phone" aria-hidden="true"></i> HOTLINE: <a href="tel: +84 967 999 999" title="(+84) 967 999 999">(+84) 967 999 999</a>
+      </div>
+      <div class="info-support">
+        <i class="fa fa-envelope" aria-hidden="true"></i> EMAIL: <a href="mailto:phonestore@gmail.com" title="phonestore@gmail.com">phonestore@gmail.com</a>
+      </div>
+    </div>
+  </div>
+</div>
